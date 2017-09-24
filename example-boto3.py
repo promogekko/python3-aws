048d817f5a0d8c05a7c1859284fa0b7d2764b5d4
Fedora Cloud User <fedora@hme-fed.novalocal>
2017-09-24 11:36:26 +0200

    first commit

---
+++ example-boto3.py
@@ -0,0 +1,13 @@
+#! /usr/bin/python3
+import boto3
+
+s3 = boto3.resource('s3')
+print(type(s3))
+
+for bucket in s3.buckets.all():
+   print(bucket.name)
+
+data = open('test.jpg', 'rb')
+s3.Bucket('my-bucket').put_object(Key='test.jpg', Body=data)
+
+
