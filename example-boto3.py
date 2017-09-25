4073184bedf5b1ead4bdd71ada1c2d0c7795d005
herve <classgekko@gmail.com>
2017-09-25 13:02:15 +0200

    changed examples

--- example-boto3.py
+++ example-boto3.py
@@ -1,13 +1,42 @@
 #! /usr/bin/python3
+
 import boto3
 
+#----------------------------------------------------------------
+#create une ressource S3
 s3 = boto3.resource('s3')
-print(type(s3))
 
+#print(type(s3))
+
+# list des buckets
 for bucket in s3.buckets.all():
    print(bucket.name)
 
-data = open('test.jpg', 'rb')
-s3.Bucket('my-bucket').put_object(Key='test.jpg', Body=data)
+# creer un S3 client pour creer un bucket
+s3_client = boto3.client('s3')
+s3_client.create_bucket(Bucket='192.134.56.78-mybucket')
 
+# ajouter une image
+data = open('test.jpg', 'rb')
+s3.Bucket('192.134.56.78-mybucket').put_object(Key="test.jpg",Body=data)
+
+#------------------------------------------------------------------------------
+sqs= boto3.resource('sqs')
+queue = sqs.create_queue(QueueName='test', Attributes={'DelaySeconds': '5'})
+
+print(queue.url)
+print(queue.attributes.get('DelaySeconds'))
+
+#------------------------------------------------------------------------------
+ec2_client = boto3.client('ec2')
+response = ec2_client.describe_instances()
+#print(response)
+
+#ec2 = boto3.resource('ec2')
+#ec2.create_instances(ImageId='ami-4fffc834', MinCount=1, MaxCount=1)
+#ec2.create_instances(ImageId='ami-4fffc834',
+#                    InstanceType='t2.micro',
+#                    KeyName='pcspecialist',
+#                    MinCount=1, MaxCount=1
+#                    )
 
